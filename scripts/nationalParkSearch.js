f502cec9dabdc38535274980788a6b5105e6500d
Adama Darboe <adadadarboe@gmail.com>
2024-05-22 07:14:24 -0400

    let do it

---
+++ scripts/nationalParkSearch.js
@@ -0,0 +1,6 @@
+console.log("--------------------------location array");
+console.log(locationsArray);
+console.log("---------------------------------National parks array----------------------------------------------");
+console.log(nationalParksArray);
+console.log("-----------------------------ParksType array--------------------------------------");
+console.log(parkTypesArray);
