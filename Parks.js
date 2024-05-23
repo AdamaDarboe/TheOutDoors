40bca1a88f6c4a0b8d52c8ab9019b032ec6e2da5
Adama Darboe <adadadarboe@gmail.com>
2024-05-23 10:06:37 -0400

    yessssss

--- Parks.js
+++ Parks.js
@@ -1,6 +1,5 @@
-console.log("--------------------------location array");
-console.log(locationsArray);
-console.log("---------------------------------National parks array----------------------------------------------");
-console.log(nationalParksArray);
-console.log("-----------------------------ParksType array--------------------------------------");
-console.log(parkTypesArray);
+"use strict";
+
+
+
+
