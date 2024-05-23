1cb4e6b0d20219f9cb1bad50f3efd20283ff7b71
Adama Darboe <adadadarboe@gmail.com>
2024-05-23 18:20:55 -0400

    wow

--- scripts/Parks.js
+++ scripts/Parks.js
@@ -1,34 +1,159 @@
+// "use strict";
+
+// const locationsArray = [
+//     "Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+//     "Delaware", "DC", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+//     "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
+//     "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
+//     "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico",
+//     "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands",
+//     "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+// ];
+
+// const  stateSelect = document.getElementById ("stateSelect");
+// const  parkTypeSelect = document.getElementById("parkTypeSelect");
+// //const submitButton = document.getElementById("submitButton");
+// const cardRowOutput = document.getElementById("cardRowOutput");
+
+
+// window.onload = () => {
+//     populateStateSelect();
+//     // console.log(nationalParksArray)
+//     populateParkType(); 
+//     console.log(parkTypeSelect)
+//     stateSelect.onchange = showLocationDataOnChang;
+    
+// //     submitButton.onclick = onsubmitButton;
+// //     stateSelect.onchange = onsubmitButton;
+//  }
+// // function onsubmitButton() {
+// //     let selectedValue = stateList.value;
+// //     if (selectedValue !== "") {
+// //         outputH1.innerHTML = selectedValue;
+// //     } else {
+// //         outputH1.innerHTML = "No Region is selected";
+// //     }
+// //}
+
+// function populateStateSelect() {
+//     for (let i = 0; i < locationsArray.length; i++) {
+//         let newOption = document.createElement("option");
+//         newOption.textContent = locationsArray[i];
+//         newOption.value = locationsArray[i];
+//         stateSelect.appendChild(newOption);
+//     }
+// }
+
+
+// const parkTypesArray = [
+//     "National Park",
+//     "National Monument",
+//     "Recreation Area",
+//     "Scenic Trail",
+//     "Battlefield",
+//     "Historic",
+//     "Memorial",
+//     "Preserve",
+//     "Island",
+//     "River",
+//     "Seashore",
+//     "Trail",
+//     "Parkway"
+// ]
+
+
+
+// function populateParkType() {
+//     for (let i = 0; i < parkTypesArray.length; i++) {
+//         let newOption = document.createElement("option");
+//         newOption.textContent = parkTypesArray[i];
+//         newOption.value = parkTypesArray[i];
+//         parkTypeSelect.appendChild(newOption);
+//     }
+// }
+
+// //After you choose
+
+
+// function showLocationDataOnChang(){
+//     cardRowOutput.innerHTML =""
+//     for(let nationalPark of nationalParksArray){
+//         console.log(stateSelect.value)
+//         if(nationalPark.State == stateSelect.value){
+//             let address =`${nationalPark.City}, ${nationalPark.State},${nationalPark.Phone}`
+//             cardRowOutput.appendChild(generateCard(nationalPark.LocationName, address))
+//             console.log("state")
+//         }
+//     }
+// }
+
+//   function showLocationDataOnChang(){
+//      cardRowOutput.innerHTML = ""
+//      for(let nationalPark of nationalParksArray){
+//         if(nationalPark.LocationName.includes(parkTypeDropdown.value) == true){
+//         let address = `${nationalPark.City},${nationalPark.State},${nationalPark.Phone}`
+//          cardRowOutput.appendChild(generateCard(nationalPark.LocationName,address));         }
+//     }}
+    
+//     function generateCard (title, location){
+//         let parkListCard = document.createElement("div");
+//         parkListCard.classList.add("card", "parkcard");
+    
+//         let parkCardBody = document.createElement("div");
+//         parkCardBody.classList.add("card-body");
+    
+//         parkListCard.appendChild(parkCardBody);
+    
+//         let parkCardTitle = document.createElement("h5");
+//         parkCardTitle.classList.add("card-title");
+    
+//         parkCardBody.appendChild(parkCardTitle);
+    
+     
+    
+//         let parkCardText = document.createElement("p");
+//         parkCardText.classList.add("card-text");
+    
+//         parkCardBody.appendChild(parkCardText);
+    
+//         let parkCardLocation = document.createElement("p");
+//         parkCardLocation.classList.add("cardLocation");
+    
+//         parkCardText.appendChild(parkCardLocation);
+    
+//         let parkCardContact = document.createElement("h6");
+//         parkCardContact.classList.add("card-text");
+    
+//         parkCardBody.appendChild(parkCardContact);
+    
+    
+//         parkCardTitle.innerHTML = title;
+    
+//         parkCardLocation.innerHTML = location;
+    
+//         // parkCardContact.innerHTML = contact
+    
+                
+            
+//         return parkListCard;
+//     }
 "use strict";
 
-const locationsArray = [
-    "Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-    "Delaware", "DC", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
-    "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
-    "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
-    "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico",
-    "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands",
-    "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
-];
+const locationsArray = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "DC", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
 
-const  stateSelect = document.getElementById ("stateSelect");
-const  parkTypeSelect = document.getElementById("parkTypeSelect");
-const submitButton = document.getElementById("submitButton");
+const stateSelect = document.getElementById("stateSelect");
+const parkTypeSelect = document.getElementById("parkTypeSelect");
+const cardRowOutput = document.getElementById("cardRowOutput");
+
 window.onload = () => {
-    populateStatesList();
-    doSomethingButton.onclick = onDoSomethingButtonClick;
-    stateList.onchange = onDoSomethingButtonClick;
-}
+    populateStateSelect();
+    populateParkType();
 
-function onDoSomethingButtonClick() {
-    let selectedValue = stateList.value;
-    if (selectedValue !== "") {
-        outputH1.innerHTML = selectedValue;
-    } else {
-        outputH1.innerHTML = "No Region is selected";
-    }
-}
+    stateSelect.onchange = showLocationDataOnChange;
+    parkTypeSelect.onchange = showLocationDataOnChange;
+};
 
-function populateStatesList() {
+function populateStateSelect() {
     for (let i = 0; i < locationsArray.length; i++) {
         let newOption = document.createElement("option");
         newOption.textContent = locationsArray[i];
@@ -37,38 +162,56 @@ function populateStatesList() {
     }
 }
 
+const parkTypesArray = ["National Park", "National Monument", "Recreation Area", "Scenic Trail", "Battlefield", "Historic", "Memorial", "Preserve", "Island", "River", "Seashore", "Trail", "Parkway"];
 
-const parkTypesArray = [
-    "National Park",
-    "National Monument",
-    "Recreation Area",
-    "Scenic Trail",
-    "Battlefield",
-    "Historic",
-    "Memorial",
-    "Preserve",
-    "Island",
-    "River",
-    "Seashore",
-    "Trail",
-    "Parkway"
-]
-
-
-function populateStatesList() {
-    for (let i = 0; i < locationsArray.length; i++) {
-        let newOption = document.createElement("option");
-        newOption.textContent = locationsArray[i];
-        newOption.value = locationsArray[i];
-        stateSelect.appendChild(newOption);
-    }
-}
-
-function populateStatesList() {
+function populateParkType() {
     for (let i = 0; i < parkTypesArray.length; i++) {
         let newOption = document.createElement("option");
         newOption.textContent = parkTypesArray[i];
         newOption.value = parkTypesArray[i];
-        stateSelect.appendChild(newOption);
+        parkTypeSelect.appendChild(newOption);
     }
 }
+
+function showLocationDataOnChange() {
+    cardRowOutput.innerHTML = "";
+    // Replace with your actual data or fetch it from somewhere
+    // const nationalParksArray = fetchNationalParksData();
+
+    for (let nationalPark of nationalParksArray) {
+        if (stateSelect.value && nationalPark.State === stateSelect.value) {
+            let address = `${nationalPark.City}, ${nationalPark.State}, ${nationalPark.Phone}`;
+            cardRowOutput.appendChild(generateCard(nationalPark.LocationName, address));
+        } else if (parkTypeSelect.value && nationalPark.LocationName.includes(parkTypeSelect.value)) {
+            let address = `${nationalPark.City}, ${nationalPark.State}, ${nationalPark.Phone}`;
+            cardRowOutput.appendChild(generateCard(nationalPark.LocationName, address));
+        }
+    }
+}
+
+function generateCard(title, location) {
+    let parkListCard = document.createElement("div");
+    parkListCard.classList.add("card", "parkcard");
+
+    let parkCardBody = document.createElement("div");
+    parkCardBody.classList.add("card-body");
+    parkListCard.appendChild(parkCardBody);
+
+    let parkCardTitle = document.createElement("h5");
+    parkCardTitle.classList.add("card-title");
+    parkCardBody.appendChild(parkCardTitle);
+    parkCardTitle.innerHTML = title;
+
+    let parkCardText = document.createElement("p");
+    parkCardText.classList.add("card-text");
+    parkCardBody.appendChild(parkCardText);
+
+    let parkCardLocation = document.createElement("p");
+    parkCardLocation.classList.add("cardLocation");
+    parkCardText.appendChild(parkCardLocation);
+    parkCardLocation.innerHTML = location;
+
+    return parkListCard;
+}
+
+    
--- scripts/mountins.js
+++ scripts/mountins.js
@@ -1,35 +1,53 @@
 "use strcit";
 
+const dropdown = document.getElementById("dropdown");
+const results = document.getElementById("results")
 
-
+window.onload = () => {
+dropdown.addEventListener('change', populateMountains)
+}
 
 const mountainSelect = document.createElement("select");
-mountainDropdown.appendChild(mountainSelect);
-mountainArry.forEach((montain, index) =>{
- const option = document.createElement("option");
- option.value = index;
 
 
- option.textContent = mountain.name;
- mountainSelect.appendChild(Option);
+mountainsArray.forEach((mountain, index) =>{
+  const option = document.createElement("option");
+  option.value = index;
+  option.textContent = mountain.name;
+  mountainSelect.appendChild(option);
 });
 
+dropdown.appendChild(mountainSelect);
+
 function populateMountains(){
-    imageHolder.innerHTML ="";
+    // imageHolder.innerHTML ="";
+   results.innerHTML = "";
+
     const selectedIndex = mountainSelect.value;
-    console.log(selectedIndex);
+    // console.log(selectedIndex);
 
 
-    let mountain = mountainArry[selectedIndex];
+    let mountain = mountainsArray[selectedIndex];
 
    console.log(mountain.desc);
-   mountainName.textContent = mountain.name;
-   mountainDesc.textContent = mountain.desc;
-   mountainElevation.textContent = mountain.elevation;
+  //  mountainName.textContent = mountain.name;
+   
+   const title = document.createElement("h1");
+   title.textContent = mountain.name;
+   results.appendChild(title);
 
- const image = document.createElement("img");
- image.scr ="images"+mountain.img;
- image.alt = mountain.name;
- imageHolder.appendChild(image);
+   const elevation = document.createElement("p");
+   elevation.textContent = mountain.elevation;
+   results.appendChild(elevation);
+
+   const description = document.createElement("p");
+   description.textContent = mountain.desc;
+   results.appendChild(description);
+
+
+  const image = document.createElement("img");
+  image.src = "images/" + mountain.img;
+  results.appendChild(image);
+
 
 }
